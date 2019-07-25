c3fd47b49989cde3eb36a2f2af8700f3d4310f47
sai1004 <saikiran.bavandla@gmail.com>
2019-07-25 17:31:46 +0530

    commit for js course folder create

---
+++ js-course/switch.js
@@ -0,0 +1,28 @@
+//SWITCH
+
+// simple spin game
+
+var slot = 90;
+
+switch (slot) {
+  case 1:
+    console.log("you won a tv");
+    break;
+  case 2:
+    console.log("you won the iphone ");
+    break;
+  case 3:
+    console.log("you won the gamming pc");
+    break;
+  case 4:
+    console.log("you won the guiter");
+    break;
+  case 5:
+    console.log("you won the bike");
+    break;
+  case 6:
+    console.log("you won the car");
+    break;
+  default:
+    console.log("ohh ohh better luck next time!!");
+}
