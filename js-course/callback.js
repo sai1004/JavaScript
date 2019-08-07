014a1b6eb74ec8088714e9a201aba43019a6c264
sai1004 <saikiran.bavandla@gmail.com>
2019-08-07 18:24:56 +0530

    commit

---
+++ js-course/callback.js
@@ -0,0 +1,13 @@
+const posts = [
+  { title: "Post One ", body: " This is Post One " },
+  { title: "Post Two ", body: " This is Post Two " },
+  { title: "Post Three ", body: " This is Post Three " }
+];
+
+function getPosts() {
+
+    this.post = this.posts
+}
+
+
+console.log(screen.width, screen.height)
---
+++ js-course/geoLocation.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("location");
+function getlocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Sorry! your browser is not supporting");
+  }
+}
+
+function showPosition(position) {
+  var x =
+    "Your current location is (" +
+    "Latitude: " +
+    position.coords.latitude +
+    ", " +
+    "Longitude: " +
+    position.coords.longitude +
+    ")";
+  document.getElementById("location").innerHTML = x;
+}
