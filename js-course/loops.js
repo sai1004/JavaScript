c30f2fe083b5275c62a7604f01038d315f8fb58a
sai1004 <saikiran.bavandla@gmail.com>
2019-08-07 19:44:49 +0530

    commit

--- js-course/loops.js
+++ js-course/loops.js
@@ -36,3 +36,27 @@ while (logged) {
   console.log(i);
   i++;
 }
+
+var list = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu"];
+
+for (let i = 0; i < list.length; i++) {
+  var openList = list[i];
+
+  console.log(openList);
+}
+
+// =====================parsing data====================\\
+
+//The JavaScript JSON.parse() takes a JSON string and
+// transforms it into a JavaScript object.
+
+var json = '{ "firstName":"ASHU", "lastName":"BHATI", "studentCode":7 }';
+var student = JSON.parse(json);
+
+console.log(student.firstName + " " + student.lastName);
+
+var dummy = '["c++","java","python" ,"ruby","perl"]';
+
+var cons = JSON.parse(dummy);
+
+console.log(cons);
