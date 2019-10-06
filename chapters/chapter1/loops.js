6f382763dee7485cc1d7d96497629487a20a227c
sai1004 <saikiran.bavandla@gmail.com>
2019-10-06 20:49:33 +0530

    commit

---
+++ chapters/chapter1/loops.js
@@ -0,0 +1,22 @@
+// ================== While loops ======================= //
+
+var count = 0;
+
+while (count <= 10) {
+  console.log(count);
+  count++;
+}
+
+// ================== do While loops ======================= //
+
+
+var text = "";
+var i = 0;
+do {
+  text += "The number is " + i + "\n";
+  i++;
+} while (i < 10);
+
+console.log(text);
+
+ 
