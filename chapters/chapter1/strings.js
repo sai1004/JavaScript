d709b7fc08f9719b197930651b3a412921877f3a
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-17 21:34:47 +0530

    Update strings.js

--- chapters/chapter1/strings.js
+++ chapters/chapter1/strings.js
@@ -1,5 +1,46 @@
 
-var sentence = 'This line is a string data type';
-// slice(start,end)
-console.log(sentence.slice(0,9))
-console.log(sentence.slice(3,-1))
+ 
+
+var string = 'To live Like a King work Hard like slave'
+
+console.log(string.split(' '))
+console.log(string.slice(0,8))
+console.log(string.replace('king','\n')) // \n breaks the line
+console.log(string.charAt(0))
+console.log(string.charCodeAt('To'))
+console.log(string.concat(' \n this is secondary string'))
+console.log(string.indexOf('live'))
+console.log(string.lastIndexOf('v'))
+// console.log(string.length(''))
+// console.log(string.localeCompare())
+console.log(string.search('l'))
+console.log(string.startsWith('To'))
+console.log(string.startsWith('To live'))
+console.log(string.endsWith('e'))
+
+
+
+var str = "hey JudE";
+var re = /[A-Z]/g;
+var re2 = /[.]/g;
+console.log(str.search(re)); // returns 4, which is the index of the first capital letter "J"
+console.log(str.search(re2)); // returns -1 cannot find '.' dot punctuation
+
+
+// var worldString = 'Hello, world';
+
+// console.log(worldString.small());     // <small>Hello, world</small>
+// console.log(worldString.big());       // <big>Hello, world</big>
+// console.log(worldString.fontsize(7)); // <font size="7">Hello, world</fontsize>
+
+// var regx = /[A-Z]/g
+// console.log(string.match(regx)) // Finding The Capital letters in string var using math and regx expression
+
+// let regexp = /t(e)(st(\d?))/g;
+// let str = 'test1 test2';
+
+// let array = [...str.matchAll(regexp)];
+
+// console.log(array[0]);
+
+
