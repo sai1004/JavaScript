d651a64f89c6ca753df154cb5ab63a3941bbee55
sai1004 <saikiran.bavandla@gmail.com>
2019-11-17 11:06:10 +0530

    commit

--- Assignments/loops.js
+++ Assignments/loops.js
@@ -1,1 +1,28 @@
+// add all even from a list , print sum of all even numbers
 
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// converting array to string
+console.log(list.toString());
+
+let evenList = [];
+
+list.forEach(item => {
+  if (item % 2 == 0) {
+    evenList.push(item);
+  }
+});
+console.log(evenList);
+
+let maxOfList = Math.max(...evenList);
+
+console.log(maxOfList);
+
+let minOfList = Math.min(...evenList);
+
+console.log(minOfList);
+
+const reducer = (accumlator, currentValue) => accumlator + currentValue;
+let sumOfList = list.reduce(reducer);
+
+console.log(sumOfList);
