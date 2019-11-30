5894853a0b8a9814ab501456285fe3c863da80c6
saikiran <35389534+sai1004@users.noreply.github.com>
2019-11-30 17:58:59 +0530

    Update operators.js

--- chapters/chapter1/operators.js
+++ chapters/chapter1/operators.js
@@ -17,17 +17,47 @@ b <= a
 
 a >= c
 
+a != c
+
+
+/* Assignment oprator */
+
 c += b  // similar to c + c = b
 
 b -= a
 
+/* Type Operators */
+
+var Bike = {make: 'Honda', model: 'CLIQ', year: 2018};
+
+// in operator
+console.log('make' in Bike);   // 
+// Output:
+// true
+
+// delete
 
 
+var Bike = { Company1: 'Honda',
+             Company2: 'Hero',
+             Company3: 'Royal Enfield'
+           };
+delete Bike.Company1;
+console.log(Bike);   // 
+// Output:
+// { Company2: 'Hero', Company3: 'Royal Enfield' }
 
-a in b
 
+// typeof
 
+let message = "Welcome to " + "JavaTpoint";
+console.log(typeof message);  // 
 
+// instanceof
+
+let arr = [1, 2, 3];
+console.log( arr instanceof Array ); // true
+console.log( arr instanceof String ); // false
 
 
 
