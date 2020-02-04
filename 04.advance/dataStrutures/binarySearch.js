549beb9c06306da9605f851ded952eac8a5786e1
saikiran <35389534+sai1004@users.noreply.github.com>
2020-02-04 23:25:28 +0530

    Create binarySearch.js

---
+++ 04.advance/dataStrutures/binarySearch.js
@@ -0,0 +1,30 @@
+const items = [1,5,2,7,3,12,6,10]
+
+// O(n) Linear
+
+// Once forEach Element in array
+
+
+items.sort((a,b) => a - b )
+
+function search(list, item){
+  let hasItem = null
+  let counter = 0
+  
+  for (let e of list) {
+    counter++
+    if(e === item) {
+      hasItem = true
+      break
+    }
+  
+  }
+  
+  console.log(counter);
+  
+  return hasItem
+  
+}
+
+
+console.log(search(items,12)) // true
