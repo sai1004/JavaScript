d36630ff742e1a0cf05547cad7f00c7bcad0385e
sai1004 <saikiran.bavandla@gmail.com>
2020-02-18 19:19:52 +0530

    commit

--- 02.Assignments/strings.js
+++ 02.Assignments/strings.js
@@ -1,8 +1,14 @@
-var para = `Lorem j Lorem`;
+// var para = `Lorem j Lorem`;
 
-console.log(para);
+// console.log(para);
 
-var wordCount = para.match(/ (\w+)/g).length;
-var space = para.match(/(\s+)/g).length;
-console.log(wordCount);
-console.log(space);
+// var wordCount = para.match(/ (\w+)/g).length;
+// var space = para.match(/(\s+)/g).length;
+// console.log(wordCount);
+// console.log(space);
+
+var task = "httpgooglecom";
+
+let part = task.replace("p", "p://").replace("e", "e.");
+
+console.log(part);
