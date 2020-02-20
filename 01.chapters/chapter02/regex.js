6d9456de271cd37f79ee74e0eeb15586c16cf391
sai1004 <saikiran.bavandla@gmail.com>
2020-02-20 19:32:41 +0530

    commit

---
+++ 01.chapters/chapter02/regex.js
@@ -0,0 +1,6 @@
+var str =
+  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eligendi aspernatur voluptatibus. Culpa, maxime asperiores enim explicabo rerum voluptate? Voluptas atque beatae consectetur maiores natus exercitationem magni aperiam deserunt iste.";
+
+var regex = /r(e)/g;
+
+console.log(str.match(regex));
