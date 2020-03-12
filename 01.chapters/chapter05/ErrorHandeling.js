d894862c65eef4fb38404b4a121bfbb6b845f5ae
sai1004 <saikiran.bavandla@gmail.com>
2020-03-12 21:03:13 +0530

    commit

--- 01.chapters/chapter05/ErrorHandeling.js
+++ 01.chapters/chapter05/ErrorHandeling.js
@@ -1,6 +1,66 @@
 try {
+  throw new Error("human-readable error message To Print!");
+} catch (error) {
+  console.log("Somthing Went Wrong! ", error.message); // calling error message
+}
+
+/* '''''''''''''''''''''''''''''''''''''' */
+
+try {
   const myVar = "dlskfjsdf";
-  myVar = 565; // const value cannot be changed. it throws error
+  // myVar = 565; // const value cannot be changed. it throws error
 } catch (err) {
   throw "Somthing Worng";
+}
+
+/* '''''''''''''''''''''''''''''''''''''' */
+
+function add(a, b) {
+  return a + b;
+}
+
+function myPerfomanceCriticalFunction() {
+  try {
+    return add(2, 4);
+  } catch (e) {
+    console.log(e);
+  }
+}
+console.log(myPerfomanceCriticalFunction());
+
+/* '''''''''''''''''''''''''''''''''''''' */
+
+function undef(c, d) {
+  return c + d;
+}
+
+try {
+  console.log(undef(4, 8));
+} catch (error) {
+  console.log("An error occurred!", error);
+} finally {
+  console.log("The code-block has finished");
+}
+
+/* '''''''''''''''''''''''''''''''''''''' */
+
+// async function main(){
+//   try {
+//     await
+//   } catch {
+
+//   }
+// }
+
+/* '''''''''''''''''''''''''''''''''''''' */
+
+try {
+  throw new TypeError();
+} catch (err) {
+  if (err instanceof Error) {
+    console.log("instance of general Error Constructor");
+  }
+  if (err instanceof TypeError) {
+    console.log("Type Error");
+  }
 }
