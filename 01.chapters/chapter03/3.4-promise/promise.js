63635d67c69dfbf594b431c2b929be36e5ce9839
saikiran <35389534+sai1004@users.noreply.github.com>
2020-07-02 08:42:00 +0530

    Update promise.js

--- 01.chapters/chapter03/3.4-promise/promise.js
+++ 01.chapters/chapter03/3.4-promise/promise.js
@@ -32,4 +32,95 @@ function sum(a, b) {
   });
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Sync Code example
+
+// function someStuff() {
+//   console.log("i am from the function");
+//   console.log("Do Some Stuff");
+// }
+
+// console.log("start");
+
+// someStuff();
+
+// console.log("End");
+
+// Async Code
+
+// console.log("start");
+
+// setTimeout(() => {
+//   console.log("We are in The Time Out!");
+// }, 2000);
+
+// console.log("End");
+
+// var x = 5;
+
+// var y = 5;
+
+// var promise = new Promise((resolve, reject) => {
+//   if (x == y) {
+//     resolve("true");
+//   } else {
+//     reject(new Error("X is Not Equals To Y"));
+//   }
+// });
+
+// promise
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const youTube = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("getting stuff from YouTube");
+//     resolve({ videos: [1, 2, 3, 4, 5, 6] });
+//   }, 2000);
+// });
+
+// const faceBook = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("stuff From FaceBook");
+//     resolve(
+//       "done"
+//     );
+//   }, 2000);
+// });
+
+// Promise.all([youTube, faceBook]).then((result) => console.log(result));
+
+const yt = () => {
+  console.log("getting stuff from YouTube");
+  return { videos: [1, 2, 3, 4, 5, 6] };
+};
+
+const fb = () => {
+  console.log("getting stuff from fb");
+  return { user: "name" };
+};
+
+async function asinkStuff() {
+  try {
+    let youTube = await yt();
+
+    console.log(youTube);
+
+    let faceBook = await fb();
+
+    console.log(faceBook);
+  } catch (err) {
+    throw err;
+  }
+}
+
+asinkStuff();
+
+
 // https://javascript.info/promise-basics
