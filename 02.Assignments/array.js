c1f85d1651eb0398e54748e4e52edf4dacb7d14a
sai1004 <saikiran.bavandla@gmail.com>
2020-08-07 17:14:58 +0530

    commit

--- 02.Assignments/array.js
+++ 02.Assignments/array.js
@@ -10,7 +10,7 @@ list.forEach((item) => {
       evenList.push(item);
    }
 });
-console.log(evenList);
+// console.log(evenList);
 
 let maxOfList = Math.max(...evenList);
 
@@ -46,4 +46,48 @@ function findInterSection(input) {
 
 var arr = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
 
-console.log(findInterSection(arr));
+// console.log(findInterSection(arr));
+
+const countries = [
+   {
+      id: "IN",
+      code: "IN",
+      name: "India",
+      lang: [
+         {
+            id: "en_hi",
+            name: "Hindi",
+            name_local: "हिन्दी",
+            active: true,
+         },
+      ],
+   },
+   {
+      id: "AE",
+      code: "AE",
+      name: "United Arab Emirates",
+      lang: [
+         {
+            id: "ar-AE",
+            name: "Arabic",
+            name_local: "عربى",
+            active: true,
+         },
+      ],
+   },
+];
+
+function findDataById(item, id) {
+   try {
+      let returnVal = {};
+      item.forEach((ele) => {
+         returnVal[ele.id] = { code: ele.code, name: ele.name, lang: JSON.stringify(ele.lang) };
+      });
+      // console.log(returnVal[`${id}`]);
+      return returnVal[`${id}`];
+   } catch (err) {
+      throw err;
+   }
+}
+
+console.log(findDataById(countries, "AE"));
