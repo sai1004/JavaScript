80a972fba333391e8887ec2d8f48438acaaa5098
sai1004 <saikiran.bavandla@gmail.com>
2021-06-04 20:59:08 +0530

    commit

--- 02.Assignments/SwitchCase.js
+++ 02.Assignments/SwitchCase.js
@@ -27,8 +27,6 @@
 //         break;
 // }
 
-
-
 var fibonacci_series = function (n) {
     if (n === 1) {
         return [0, 1];
@@ -39,4 +37,43 @@ var fibonacci_series = function (n) {
     }
 };
 
-console.log(fibonacci_series(8));
+// console.log(fibonacci_series(8));
+
+let str = "aaaabbbccccddde";
+
+function runLengthEncode(str) {
+    let newStr = "";
+
+    let num = 1;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == str[i + 1]) {
+            num++;
+        } else {
+            newStr = newStr + num + str[i];
+            num = 1;
+        }
+    }
+
+    return newStr;
+}
+
+// console.log(runLengthEncode(str));
+
+function twoSum(nums, target) {
+    let res = []; // this will store our result
+    for (let i = 0; i < nums.length; i++) {
+        // here we are looping through the array
+        for (let j = i + 1; j < nums.length; j++) {
+            // here also, looping through the array but leaving the first element
+            if (nums[i] + nums[j] === target) {
+                res.push(i);
+                res.push(j);
+            }
+        }
+    }
+    return res;
+}
+// [1,2,3,5] // 4
+// [0,2]
+console.log(twoSum([1, 2, 3, 5], 6));
